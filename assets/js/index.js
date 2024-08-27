c62ff96eb23a9bc544df7f3ac0ef27a241db8f20
marutaaze <117014134+marutaaze@users.noreply.github.com>
2024-08-27 11:46:57 +0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+/*
+下方为toppage最上方的banner  
+    排列顺序：最新的放在最前面
+    link:  banner对应跳转链接
+    pc_img_src: PC端banner图片地址
+    mob_img_src: 手机端banner图片地址
+*/    
+var banner = [
+    {
+        link:'https://www.rakuten.ne.jp/gold/chiclab/',
+        pc_img_src:'images/202307/toppage_banner01.png',
+        mob_img_src:'images/202307/popup.png'
+    },
+    {
+        link:'https://www.rakuten.ne.jp/gold/chiclab/',
+        pc_img_src:'images/202307/toppage_banner02.png',
+        mob_img_src:'images/202307/popup.png'
+    },
+    {
+        link:'https://www.rakuten.ne.jp/gold/chiclab/',
+        pc_img_src:'images/202307/toppage_banner03.png',
+        mob_img_src:'images/202307/popup.png'
+    },
+    {
+        link:'https://www.rakuten.ne.jp/gold/chiclab/',
+        pc_img_src:'images/202307/toppage_banner04.png',
+        mob_img_src:'images/202307/popup.png'
+    },
+    {
+        link:'https://www.rakuten.ne.jp/gold/chiclab/',
+        pc_img_src:'images/202307/toppage_banner05.png',
+        mob_img_src:'images/202307/popup.png'
+    },
+]
